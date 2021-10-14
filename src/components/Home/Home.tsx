1e5da60555f2b70b896dc53053f8d496059b63f9
eabsin <eleizer.absin@gmail.com>
2021-10-14 11:51:01 +0800

    initial code - tailwind

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <button type="button">Connect</button>
+      <p>Hello Home</p>
+    </div>
+  );
+};
+
+export default Home;
